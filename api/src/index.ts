5570cd4e8957a767e02956be2609391f6ef9c404
Danny_Young <danielbampoeocran50@gmail.com>
2024-11-29 04:46:20 +0000

    basic setup

---
+++ api/src/index.ts
@@ -0,0 +1,18 @@
+import express, { json,urlencoded } from 'express';
+//import productRoutes from './routes/products/index';
+const port = 3000
+const app = express();
+
+app.use(urlencoded({extended:false}))
+app.use(json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+
+//app.use('/products', productRoutes);
+app.listen(port,() => {
+    console.log(`Server running on port ${port}`);
+});
+ 
